a9ed7dd9312ffcd29707fb6c80601b7fbc0ec677
4thdraw <4thdraw2@gmail.com>
2023-11-06 12:08:35 +0900

    fade처리

--- js/web.js
+++ js/web.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
-    // 화면이 준비되면 실행해라.
-    // setInterval , animate, css , appendTo 
-    // setInterval(function(){}, 3000)
-    // $(태그).animate({}, speed, function(){})
-    // $(태그).css(스타일, 스타일값)
-    // $(옮길테그).appendTo($(부모가될태그))
 
-    const aniw = $(".swiper-slide").height();
+    let count = 0;
 
+    setInterval(fadeani, 3000)
 
-    setInterval(function () {
-        $(".swiper-wrapper").animate({ "marginTop": -aniw }, 400, function () {
-            $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
-            $(".swiper-wrapper").css("marginTop", 0)
-        })
-    }, 3000)
+    function fadeani() { //선언적함수
+        count++;
+        count = count % $(".swiper-slide").length;
+        // $(".swiper-slide").removeClass("on")
+        $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on")
+    }
 
 })
+
