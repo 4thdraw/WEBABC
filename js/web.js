34d6aaf5d01b5b45f88e016873f4fbd50ee9149e
4thdraw <4thdraw2@gmail.com>
2023-11-06 11:03:13 +0900

    상하슬라이드

--- js/web.js
+++ js/web.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
     // $(태그).css(스타일, 스타일값)
     // $(옮길테그).appendTo($(부모가될태그))
 
-    const aniw = $(".swiper-slide").width(); // $(".swiper-slide").css("width");
+    const aniw = $(".swiper-slide").height();
 
 
     setInterval(function () {
-        $(".swiper-wrapper").animate({ "marginLeft": -aniw }, 400, function () {
+        $(".swiper-wrapper").animate({ "marginTop": -aniw }, 400, function () {
             $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
-            $(".swiper-wrapper").css("marginLeft", 0)
+            $(".swiper-wrapper").css("marginTop", 0)
         })
     }, 3000)
 
